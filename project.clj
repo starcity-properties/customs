aac6b7b8fe45a63b845b23a90d9532794244fff4
Josh Lehman <jalehman37@gmail.com>
2018-09-24 18:03:10 -0700

    Version 1.2.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/customs "1.2.1"
+(defproject starcity/customs "1.2.2-SNAPSHOT"
   :description "Starcity authentication and authorization utilities"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}

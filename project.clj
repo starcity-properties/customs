93be4a2cb64adf020d48db13d1170cbc25f3d4a6
James Muturi <muturijames3@gmail.com>
2020-05-13 18:23:46 +0300

    Version 1.3.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/customs "1.3.1"
+(defproject starcity/customs "1.3.2-SNAPSHOT"
   :description "Starcity authentication and authorization utilities"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
